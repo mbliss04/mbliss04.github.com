243e5f30ac2bfdbe34e39fb29095650ed8a83272
McCall Bliss <missmccallkay@gmail.com>
2013-05-22 18:35:40 -0500

    new distance formula

--- redline/script/world.js
+++ redline/script/world.js
@@ -258,7 +258,7 @@ function drawPolyline(map) {
       Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
       var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
       var d = R * c;
-      return d;
+      return d.toPrecision(4);
   }
 
 	
