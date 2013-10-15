3c3721aa6e9d14fed0f7972414cad4b7e0bd96f0
McCall Bliss <missmccallkay@gmail.com>
2013-10-15 22:41:24 +1300

    fix hover on portfolio section

--- js/script.js
+++ js/script.js
@@ -24,10 +24,10 @@ $(document).ready(function(){
 	/* --- jQuery mouseover portfolio section --- */
 
 	$('.viewport').mouseenter(function(e) {
-        $(this).children('a').children('img').animate({ height: '200', left: '-10', top: '-10', width: '300'}, 100);
+        $(this).children('a').children('img').animate({ height: '220', left: '0', top: '-10', width: '330'}, 100);
         $(this).children('a').children('span').fadeIn(200);
     }).mouseleave(function(e) {
-        $(this).children('a').children('img').animate({ height: '220', left: '-20', top: '-20', width: '320'}, 100);
+        $(this).children('a').children('img').animate({ height: '200', left: '0', top: '0', width: '300'}, 100);
         $(this).children('a').children('span').fadeOut(200);
     });
 
